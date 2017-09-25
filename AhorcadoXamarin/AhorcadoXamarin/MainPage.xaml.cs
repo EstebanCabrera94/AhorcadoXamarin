a5660c13fd751e201205a0be9f85c51b06a4e454
EstebanCabrera94 <estebancabrera@hotmail.es>
2017-09-25 17:27:09 +0100

    teclado e imagen

--- AhorcadoXamarin/AhorcadoXamarin/MainPage.xaml.cs
+++ AhorcadoXamarin/AhorcadoXamarin/MainPage.xaml.cs
@@ -9,9 +9,41 @@ namespace AhorcadoXamarin
 {
     public partial class MainPage : ContentPage
     {
+        readonly string[] ALFABETO = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
         public MainPage()
         {
             InitializeComponent();
+
+            Inicializar();
+        }
+
+        private void Inicializar()
+        {
+            imageAhorcado.Source = ImageSource.FromResource("AhorcadoXamarin.img.1.png");
+            int letra = 0;
+            for (int fila = 0; fila < 5; fila++)
+            {
+                for (int columna = 0; columna < 6; columna++)
+                {
+                    gridBotonera.Children.Add(new Button
+                    {
+                        Text = ALFABETO[letra],
+                    }, columna, fila);
+                    letra++;
+                    if (letra == ALFABETO.Length)
+                    {
+                        break;
+                    }
+                }
+                   
+
+                /*
+                Button boton = new Button();
+                boton.Text = ALFABETO[columna];
+                gridBotonera.Children.Add(boton, columna, fila);
+                */
+            }
+            
         }
     }
 }
